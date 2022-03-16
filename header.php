d0e2546573975a0262d01a61edb34cbc3153c7e6
Thomas Milton <perso.milton@gmail.com>
2022-03-16 11:25:40 +0100

    premier commit

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+  <title>Mon site</title>
+</head>
+
+<body <?php body_class(); ?>>
+  <header class="site-header">
+    <h1><?php bloginfo('name'); ?></h1>
+
+  </header>
